3086638ce25f98ac1a6ee67341193c10c456c53e
happycodez <happybrown2k@gmail.com>
2024-09-19 11:42:25 +0200

    first commit

---
+++ constant/index.ts
@@ -0,0 +1,22 @@
+export const portfolioRouters = [
+    {
+        title: "Home",
+        path: "/",
+    },
+    {
+        title: "About",
+        path: "/about",
+    },
+    {
+        title: "Tech Stack",
+        path: "/tech-stack",
+    },
+    {
+        title: "Projects",
+        path: "/projects",
+    },
+    {
+        title: "Contact",
+        path: "/contact",
+    },
+];
